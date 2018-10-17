e754e98b945e9d7bdd808aa09df6947fbbf76423
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2018-10-17 15:39:09 -0700

    ENH: Add first pass of unit tests

--- q2_ili/tests/test_plot.py
+++ q2_ili/tests/test_plot.py
@@ -14,20 +14,36 @@ import pandas as pd
 import qiime2 as q2
 
 
-from q2_ili import plot
+from q2_ili import plot, STLDirFmt
 
 
 class PlotTests(unittest.TestCase):
     def setUp(self):
-        self.model = None
+        # avoid including an STL model in the repo since we only really need
+        # to test that the copying works, this can just be a file
+        self.stl = tempfile.NamedTemporaryFile()
+
+        self.model = STLDirFmt(path=self.stl.name, mode='r')
         self.metadata = q2.Metadata(
             pd.DataFrame({'val1': ['1.0', '2.0', '3.0', '4.0'],
                           'val2': ['3.3', '3.5', '3.6', '3.9']},
                          index=pd.Index(['A', 'B', 'C', 'D'], name='id')))
 
+    def tearDown(self):
+        self.deletable.close()
+
     def test_plot(self):
         with tempfile.TemporaryDirectory() as output_dir:
             plot(output_dir, self.model, self.metadata)
             index_fp = os.path.join(output_dir, 'index.html')
+
+            # check the base HTML is copied
             self.assertTrue(os.path.exists(index_fp))
-            self.assertTrue('src="./ili.html"' in open(index_fp).read())
+            self.assertTrue('Spatial Data Mapping' in open(index_fp).read())
+
+            model_fp = os.path.join(output_dir, 'js', 'workers', 'model.stl')
+            metadata_fp = os.path.join(output_dir, 'js', 'workers',
+                                       'features.csv')
+            # check the metadata and the model are properly copied
+            self.assertTrue(os.path.exists(model_fp))
+            self.assertTrue(os.path.exists(metadata_fp))
